50f61d46f922972226f1983ea64b74a83e770c78
Tj Holowaychuk <tj@vision-media.ca>
2011-07-29 12:47:15 -0700

    setting up assert.response

--- test/assert.js
+++ test/assert.js
@@ -3,4 +3,11 @@
  * Module dependencies.
  */
 
-var assert = require('assert');
+var assert = require('assert')
+  , http = require('http');
+
+module.exports = assert;
+
+assert.response = function(app, req, res){
+  console.log(req);
+};
